db3038c141255baebcd61bcf63ee35474c967b29
Harvey Ducay <ducs1007@gmail.com>
2024-09-26 10:26:43 +0800

    removing next image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import { StickyWallComponent } from "@/components/sticky-wall";
 export default function Home() {
-  
+
   return (
     <StickyWallComponent />
   );
